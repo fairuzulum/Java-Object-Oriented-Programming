09109a52d92476e1c8e03822132ff65e4a8e7866
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-31 16:22:47 +0700

    [ADD]membat package

---
+++ src/Fairuz/com/data/Product.java
@@ -0,0 +1,11 @@
+package Fairuz.com.data;
+
+class Product {
+    String name;
+    int price;
+
+    Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
