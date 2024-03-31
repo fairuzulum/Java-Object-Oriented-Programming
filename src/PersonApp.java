9339ccd90609c6ca9e71faac4511efc98686a33e
Ahmad Fairuz Ulumuddin <usleunmdi32@gmail.com>
2024-03-31 14:50:24 +0700

    [ADD] membuat field dan final field

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,6 +1,11 @@
 public class PersonApp {
     public static void main(String[] args) {
         Person person = new Person(); // membuat object
-        System.out.println(person);
+        person.name = "Fairuz";
+        person.address = "Bekasi";
+//        person.country = "Jepang"; // Erro karena final
+        System.out.println(person.name);
+        System.out.println(person.address);
+        System.out.println(person.country);
     }
 }
